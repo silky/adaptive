a85d607acd80e1bbe6ee3a421e54c85c1a5ea97d
Bas Nijholt <basnijholt@gmail.com>
2018-06-07 21:08:45 -0700

    do not always plot an Overlay because HoloViews can now handle changing datatypes in DynamicMaps

--- adaptive/learner/learner2D.py
+++ adaptive/learner/learner2D.py
@@ -413,8 +413,6 @@ class Learner2D(BaseLearner):
                                 mode='constant',
                                 constant_values=np.nan).reshape(-1, 2)
                 tris = hv.EdgePaths([points])
-            else:
-                tris = hv.EdgePaths([])
         else:
             im = hv.Image([], bounds=lbrt)
             tris = hv.EdgePaths([])
@@ -423,4 +421,7 @@ class Learner2D(BaseLearner):
         tri_opts = dict(line_width=0.5, alpha=tri_alpha)
         no_hover = dict(plot=dict(inspection_policy=None, tools=[]))
 
-        return im.opts(style=im_opts) * tris.opts(style=tri_opts, **no_hover)
+        if tri_alpha:
+            return im.opts(style=im_opts) * tris.opts(style=tri_opts, **no_hover)
+        else:
+            return im.opts(style=im_opts)
