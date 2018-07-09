ba110abf224243048e7e1d32515519f2df78b1f2
Joseph Weston <joseph@weston.cloud>
2018-07-09 18:23:25 +0200

    add SKOptLearner test
    A truly trivial test, but there's not much we can guarantee about
    the SKOptLearner.

--- adaptive/tests/test_learner.py
+++ adaptive/tests/test_learner.py
@@ -112,6 +112,22 @@ def ask_randomly(learner, rounds, points):
     return xs, ls
 
 
+def test_skopt_learner_runs():
+    """The SKOptLearner provides very few guarantees about its
+       behaviour, so we only test the most basic usage
+    """
+
+    def g(x, noise_level=0.1):
+        return (np.sin(5 * x) * (1 - np.tanh(x ** 2))
+                + np.random.randn() * noise_level)
+
+    learner = SKOptLearner(g, dimensions=[(-2., 2.)])
+
+    for _ in range(11):
+        (x,), _ = learner.ask(1)
+        learner._tell(x, learner.function(x))
+
+
 @run_with(Learner1D)
 def test_uniform_sampling1D(learner_type, f, learner_kwargs):
     """Points are sampled uniformly if no data is provided.
